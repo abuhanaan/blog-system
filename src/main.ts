035a68f7b19f6aa7fd988ccdcd2d195420e4a3e4
abuhanaan <adewunmi99@gmail.com>
2023-12-31 02:11:28 +0100

    Implemented Input Validation on Post Endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   // SwaggerUI Setup
   const config = new DocumentBuilder()
@@ -11,9 +14,9 @@ async function bootstrap() {
     .setDescription('The Blog API description')
     .setVersion('0.1')
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
